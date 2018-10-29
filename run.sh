0ec02165b041426b3c459ba27efdb2fbdfe1043b
Talits <talita.bernardesp@gmail.com>
2018-10-29 17:36:11 -0300

    change var to environment var

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
     today=`date +%D`
-    expiredate=`echo | openssl s_client -connect {{openshift_master_default_subdomain}}:443 2>/dev/null | openssl x509 -noout -dates| cut -d '=' -f2 | tail -n1`
+    expiredate=`echo | openssl s_client -connect ${endpoint}:443 2>/dev/null | openssl x509 -noout -dates| cut -d '=' -f2 | tail -n1`
     expdate="date +%D --date='$expiredate'"
     ed=`eval $expdate`
     daysleft=`echo $(($(($(date -u -d "$ed" "+%s") - $(date -u -d "$today" "+%s"))) / 86400))`
-    text="{{ openshift_master_default_subdomain }}- expiring on: $ed , $daysleft days left to go."
+    text="${endpoint}- expiring on: $ed , $daysleft days left to go."
     echo $expiredate
 
     if [ $daysleft -le 60 ]
