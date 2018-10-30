9e9902fad86389559d36dd3d27f48f23990d7b84
Talits <talita.bernardesp@gmail.com>
2018-10-30 09:59:51 -0300

    change env vars

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
     today=`date +%D`
-    expiredate=`echo | openssl s_client -connect ${endpoint}:443 2>/dev/null | openssl x509 -noout -dates| cut -d '=' -f2 | tail -n1`
+    expiredate=`echo | openssl s_client -connect ${ENDPOINT}:443 2>/dev/null | openssl x509 -noout -dates| cut -d '=' -f2 | tail -n1`
     expdate="date +%D --date='$expiredate'"
     ed=`eval $expdate`
     daysleft=`echo $(($(($(date -u -d "$ed" "+%s") - $(date -u -d "$today" "+%s"))) / 86400))`
-    text="${endpoint}- expiring on: $ed , $daysleft days left to go."
+    text="${ENDPOINT} - expiring on: $ed , $daysleft days left to go."
     echo $expiredate
 
     if [ $daysleft -le 60 ]
     then
-          webhook_url='${webhook_url}'
-          channel='${channel}'
-          json="{\"channel\": \"${channel}\", \"username\":\"certificates\", \"icon_emoji\":\":scream_cat:\", \"attachments\":[{\"color\":\"danger\" , \"text\": \"$text\"}]}"
+          webhook_url='${WEBHOOK_URL}'
+          channel='${CHANNEL}'
+          json="{\"channel\": \"${CHANNEL}\", \"username\":\"certificates\", \"icon_emoji\":\":scream_cat:\", \"attachments\":[{\"color\":\"danger\" , \"text\": \"$text\"}]}"
           curl -X POST --data-urlencode  "payload=$json" "$webhook_url"
     fi
 
